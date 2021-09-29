a6bf95f99933b446e8824700f8ebfa3c598a8db6
siavash <siavashj90>
2021-09-29 15:09:47 -0700

    Added PostgreSQL and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -6,7 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'siavash'
 api = Api(app)
@@ -23,5 +24,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister,'/register')
 
 if __name__ == '__main__':
-
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
